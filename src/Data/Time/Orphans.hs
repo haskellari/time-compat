d116ef916813150c208a7568037a74c6a67fd901
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-05-14 13:59:09 +0300

    Require GHC-7.4+, time-1.4+

--- src/Data/Time/Orphans.hs
+++ src/Data/Time/Orphans.hs
@@ -31,38 +31,6 @@ import Data.Time.Calendar.Month
 import Data.Time.Calendar.Quarter
 #endif
 
-#if !MIN_VERSION_time(1,4,0)
-instance NFData Day where
-    rnf (ModifiedJulianDay d) = rnf d
-
-instance NFData UniversalTime where
-    rnf (ModJulianDate d) = rnf d
-
-instance NFData DiffTime where
-    rnf d = d `seq` ()
-
-instance NFData AbsoluteTime where
-    rnf d = d `seq` ()
-
-instance NFData UTCTime where
-    rnf (UTCTime d t) = rnf d `seq` rnf t
-
-instance NFData NominalDiffTime where
-    rnf d = d `seq` ()
-
-instance NFData LocalTime where
-    rnf (LocalTime d tod) = rnf d `seq` rnf tod
-
-instance NFData ZonedTime where
-    rnf (ZonedTime lt tz) = rnf lt `seq` rnf tz
-
-instance NFData TimeOfDay where
-    rnf (TimeOfDay h m s) = rnf h `seq` rnf m `seq` rnf s
-
-instance NFData TimeZone where
-    rnf (TimeZone a b c) = rnf a `seq` rnf b `seq` rnf c
-#endif
-
 #if !MIN_VERSION_time(1,6,0)
 instance ParseTime UniversalTime where
     -- substituteTimeSpecifier _ = timeSubstituteTimeSpecifier
